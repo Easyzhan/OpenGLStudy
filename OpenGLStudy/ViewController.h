b9159bf54053d0c4bf10df4e0756f33e9a8efbce
Vizard <weida.zhan@tqmall.com>
2017-02-23 15:54:32 +0800

    OpenGLDemoOne

---
+++ OpenGLStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLStudy
+//
+//  Created by Zin_戦 on 17/2/23.
+//  Copyright © 2017年 Zin_戦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+@interface ViewController : GLKViewController
+
+
+@end
+
